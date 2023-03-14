225e3720c27ef0bf7df3e8d38a4a660356627751
Youness <youness@adaptivity.nl>
2023-03-14 23:38:33 +0100

    first commit

---
+++ src/FileUploaderClient.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.net.*;
+
+public class FileUploaderClient {
+    static final int PORT = 5656;
+
+    public static void main(String[] args) {
+        MultiThreadedClient thread = new MultiThreadedClient();
+        thread.run();
+    }
+
+    public static void progressBar(int percentage) {
+        int length = 26;
+        int filledLength = (int) (percentage / 100.0 * length);
+        int remainingLength = length - filledLength;
+
+        String sb = '\r' +
+                String.format("%3d%% [", percentage) +
+                "=".repeat(Math.max(0, filledLength)) +
+                " ".repeat(Math.max(0, remainingLength)) +
+                ']';
+
+        System.out.print(sb);
+    }
+
+    public static class MultiThreadedClient implements Runnable {
+        private Socket socket;
+//        private PrintWriter out;
+//        private BufferedReader in;
+        private ObjectOutputStream objectOutputStream;
+        private ObjectInputStream objectInputStream;
+
+        public static void main(String[] args) {
+            Thread clientThread = new Thread(new MultiThreadedClient());
+            clientThread.start();
+        }
+
+        public void run() {
+            try {
+                socket = new Socket("localhost", PORT);
+                objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+                System.out.println("Vul hier het bestandspad in: ");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String filePath = bufferedReader.readLine();
+
+                File file = new File(filePath);
+                if (!file.exists() || !file.isFile()) {
+                    System.err.println("Bestand kon niet gevonden worden");
+                    System.exit(-1);
+                }
+
+                objectOutputStream.writeObject(file.getName());
+                objectOutputStream.writeObject(file.length());
+
+                byte[] buffer = new byte[4096];
+                float filesize = file.length();
+                float chunks = filesize / (float) buffer.length;
+
+                int bytesRead;
+                int chunkIndex = 0;
+
+                FileInputStream fileInputStream = new FileInputStream(file);
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    objectOutputStream.write(buffer, 0, bytesRead);
+                    chunkIndex++;
+
+                    float percentage = chunkIndex / chunks * 100;
+                    progressBar((int) percentage);
+                }
+
+                System.out.println('\n');
+
+                objectOutputStream.close();
+                objectInputStream.close();
+                fileInputStream.close();
+                bufferedReader.close();
+
+                System.out.println("Bestand succesvol verzonden!");
+            } catch (IOException e) {
+                System.err.println("Kon geen verbinding maken met " + socket.getInetAddress().getHostName() + ":" + PORT);
+                System.exit(-1);
+            }
+        }
+    }
+}
---
+++ src/FileUploaderServer.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.net.*;
+
+public class FileUploaderServer {
+    static final int PORT = 5656;
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            System.err.println("Kon geen server starten op poort: " + PORT);
+            System.exit(-1);
+        }
+
+        System.out.println("--- Server draait en luister naar poort " + PORT + " ---");
+
+        Socket clientSocket;
+
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                System.out.println("Nieuwe client verbonden: " + clientSocket.getInetAddress().getHostAddress());
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+
+            } catch (IOException e) {
+                System.err.println("Fout bij verbinding met client: " + e.getMessage());
+                System.exit(-1);
+            }
+        }
+    }
+
+    public static class ClientHandler implements Runnable {
+        private final Socket socket;
+        private final ObjectOutputStream objectOutputStream;
+        private final ObjectInputStream objectInputStream;
+
+        ClientHandler (Socket socket) throws IOException {
+            this.socket = socket;
+            this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            this.objectInputStream = new ObjectInputStream(socket.getInputStream());
+        }
+
+        @Override
+        public void run() {
+            String filePath = System.getProperty("user.dir") + "\\uploaded-files\\";
+
+            File folderFile = new File(filePath);
+            if (!folderFile.exists() && !folderFile.mkdir()) {
+                System.err.println("Kon geen map aanmaken voor de bestanden.");
+                System.exit(-1);
+            }
+
+            String filename = null;
+            FileOutputStream fileOutputStream = null;
+
+            try {
+                filename = (String) objectInputStream.readObject();
+                long filesize = (long) objectInputStream.readObject();
+                System.out.println("Nieuw bestand wordt geüpload: " + filename + " (Bestandsgrootte: " + filesize + " bytes)");
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                long bytesReceived = 0;
+
+                fileOutputStream = new FileOutputStream(filePath + filename);
+                while (bytesReceived < filesize && (bytesRead = objectInputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead);
+                    bytesReceived += bytesRead;
+                }
+
+                fileOutputStream.close();
+                System.out.println("Bestand succesvol ontvangen!");
+
+            } catch (IOException | ClassNotFoundException e) {
+                System.err.println("Verbinding verbroken met client");
+                if (filename != null && fileOutputStream != null) {
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                    File file = new File(filePath + filename);
+                    if (file.exists()) {
+                        if (!file.delete()) {
+                            System.err.println("Corrupt bestand kon niet worden verwijderd.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
